e599defb6b67f25f4b99b8747b4822439d303c71
MatheusJosé <matheusfairy0@gmail.com>
2025-08-27 12:47:52 -0300

    Update scripts.ts

--- scripts.ts
+++ scripts.ts
@@ -1,15 +1,33 @@
-interface SiteStatus{
-    caminho: string;
-    status: boolean;
+import fetch from "node-fetch";
+
+type Resultado = {
+    site: string;
+    status: number | string;
+};
+
+async function verificarSites(sites: string[]): Promise<void> {
+    const promessas: Promise<Resultado>[] = sites.map(async site => {
+        try {
+            const resultados = await fetch(site);
+            return { site, status: resultados.status };
+        } catch (error: any) {
+            return { site, status: `Erro: ${error.code || error.message}` };
+        }
+    });
+
+    const resultados: Resultado[] = await Promise.all(promessas);
+
+    console.log("Status dos Sites:");
+    resultados.forEach(res => {
+        console.log(`${res.site}: Status ${res.status}`);
+    });
 }
 
-function verificarSites(caminho: string): Promise<SiteStatus>{
-    console.log("Verificando Sites...");
-    return new Promise((resolve , reject) =>{
-        setTimeout(() =>{
-            if(caminho)
-        }
-    }
-}
-const sites: string[] = ["https://www.google.com", "https://www.github.com",];
+const sites: string[] = [
+    "https://www.google.com",
+    "https://www.github.com",
+    "https://umsitequenaoexiste.com.br"
+];
+
+// Chama a função
 verificarSites(sites);
